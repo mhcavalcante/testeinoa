d4ab1e48a5143361cf648d39681137e210ae50fc
Mateus Cavalcante <mateuscavalcante@gmail.com>
2023-06-03 13:24:38 -0300

    criacao dos Models e configuracao das models no template de admin

--- testeinoa/admin.py
+++ testeinoa/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from testeinoa.models import Stock, PriceHistory
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('stock_code', 'tunel_max','tunel_min','update_frequency', 'email')
+
+class PriceHistoryAdmin(admin.ModelAdmin):
+    list_display = ('stock_code', 'price', 'update_time', 'stock')
+
+admin.site.register(Stock, StockAdmin)
+admin.site.register(PriceHistory, PriceHistoryAdmin)
--- testeinoa/models.py
+++ testeinoa/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Stock(models.Model):
+
+    stock_code = models.CharField('Código do Ativo',
+                                  max_length=20,
+                                  unique=True)
+    
+    tunel_max = models.DecimalField('Túnel Max',
+                                    max_digits=10,
+                                    decimal_places=2,
+                                    validators=[MinValueValidator(0.001, 'Túnel máximo deve ser maior que R$ 0,00.')])
+    
+    tunel_min = models.DecimalField('Túnel Min',
+                                    max_digits=10,
+                                    decimal_places=2,
+                                    validators=[MinValueValidator(0.001, 'Túnel mínimo deve ser maior que R$ 0,00')])
+
+    update_frequency = models.IntegerField('Periodicidade de atualização (minutos)',
+                                            validators=[MinValueValidator(1, 'Periodicidade deve ser no mínimo de 1 minuto.')])
+
+    email = models.EmailField(max_length=255)
+    
+class PriceHistory(models.Model):
+    
+    stock_code = models.CharField('Código do Ativo',
+                                  max_length=20)
+    
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    
+    update_time = models.DateTimeField('Data da atualização')
+    
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
